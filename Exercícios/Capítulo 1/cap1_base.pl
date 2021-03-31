37754b9bd43499f639cf00781b02dea952410df7
Rafael da Silva Fernandes <32852162+RafaelxFernandes@users.noreply.github.com>
2021-03-31 18:18:45 -0300

    + exercícios semana 2 aula 01/04

--- Exercícios/Capítulo 1/cap1_base.pl
+++ Exercícios/Capítulo 1/cap1_base.pl
@@ -4,3 +4,41 @@ parent(tom, liz).
 parent(bob, ann).
 parent(bob, pat).
 parent(pat, jim).
+parent(jim, karl).
+
+female(pam).
+female(liz).
+female(pat).
+female(ann).
+male(tom).
+male(bob).
+male(jim).
+
+offspring(Y,X):-
+	parent(X,Y).
+
+mother(X,Y):-
+	parent(X,Y),
+	female(X).
+
+grandparent(X,Z):-
+	parent(X,Y),
+	parent(Y,Z).
+
+sister(X,Y):-
+	parent(Z,X),
+	parent(Z,Y),
+	female(X).
+
+
+predecessor(X,Y):- predecessor(X,Y).
+predecessor(X,Y):- predecessor(X,Z), predecessor(Z,Y).
+
+ancestral2(X,Y):- parent(X,Z), ancestral2(Z,Y).
+ancestral2(X,Y):- parent(X,Y).
+
+ancestral3(X,Y):- parent(X,Y).
+ancestral3(X,Y):- ancestral3(X,Z), parent(Z,Y).
+
+ancestral4(X,Y):- ancestral4(X,Z), parent(Z,Y).
+ancestral4(X,Y):- parent(X,Y).
