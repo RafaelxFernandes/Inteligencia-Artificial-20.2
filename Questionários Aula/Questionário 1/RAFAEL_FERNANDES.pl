dd09d9986f03c53a8ff65589be29743893b0ebfa
Rafael da Silva Fernandes <32852162+RafaelxFernandes@users.noreply.github.com>
2021-04-12 13:09:12 -0300

    * correção questionário 1 | + regra meio-irmão

--- Questionários Aula/Questionário 1/RAFAEL_FERNANDES.pl
+++ Questionários Aula/Questionário 1/RAFAEL_FERNANDES.pl
@@ -15,10 +15,32 @@ male(jim).
 father(X,Y) :- male(X), parent(X,Y).
 mother(X,Y) :- female(X), parent(X,Y).
 
-% Resolução do exercício
+not(P):- P, !, fail.
+not(P).
+
+% Resolução do exercício em aula
 irmaos(X,Y):-
 	father(F,X),
 	father(F,Y),
 	mother(M,X),
 	mother(M,Y).
-	
+
+% Correção do exercício
+irmaosCorrigido(X,Y):-
+	father(F,X),
+	father(F,Y),
+	mother(M,X),
+	mother(M,Y),
+	X \= Y. % <==========
+
+% Resolução usando cut e negação
+// Perguntar
+
+% Meio-irmão
+meioIrmao(X, Y):-
+	X \= Y,
+	not(irmaosCorrigido(X,Y)),
+	(father(F,X),
+	father(F,Y);
+	mother(M,X),
+	mother(M,Y)).
