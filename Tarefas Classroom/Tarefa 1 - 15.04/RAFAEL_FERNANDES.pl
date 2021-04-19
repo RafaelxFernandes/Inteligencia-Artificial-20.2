c48f107ebef89fc93cdb75580668d8e0bed23a57
Rafael da Silva Fernandes <32852162+RafaelxFernandes@users.noreply.github.com>
2021-04-19 15:33:43 -0300

    + exemplos consultas tarefa 1

--- Tarefas Classroom/Tarefa 1 - 15.04/RAFAEL_FERNANDES.pl
+++ Tarefas Classroom/Tarefa 1 - 15.04/RAFAEL_FERNANDES.pl
@@ -5,6 +5,11 @@ prog1(X, [Head1 | Tail1], [Head2 | Tail2]):-
 	Head2 is (X + Head1),
 	prog1(X, Tail1, Tail2).
 
+% Exemplo consulta:
+% ?- prog1(1, [1,2,3], L2).
+% L2 = [2, 3, 4] ;
+% false.
+
 
 % Exercício 2 
 
@@ -12,14 +17,24 @@ prog2(X, [0]):- X >=0, X < 4.
 
 % regra criada para o caso em que Mod for 0 (zero)
 prog2(X, [Head | Tail]):- 
-	X >= 4,
-	0 is mod(X, 4),
-	Resultado is (X - 4),
-	prog2(Resultado, Tail).
+  X >= 4,
+  0 is mod(X, 4),
+  Resultado is (X - 4),
+  Head = X,
+  prog2(Resultado, Tail).
 
 prog2(X, L):-
-	X >= 4,
-	Mod is mod(X, 4),
-	Mod > 0,
-	Resultado is (X - Mod),
-	prog2(Resultado, L).
+  X >= 4,
+  Mod is mod(X, 4),
+  Mod > 0,
+  Resultado is (X - Mod),
+  prog2(Resultado, L).
+
+% Exemplo consulta:
+% ?- prog2(8, L).
+% L = [8, 4, 0] ;
+% false.
+
+% ?- prog2(9, L).
+% L = [8, 4, 0] ;
+% false.
